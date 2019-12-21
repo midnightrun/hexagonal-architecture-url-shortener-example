1c1d397f57f2986ed4d76e5d60c779e5d18faad4
Camal Cakar <camal_cakar@yahoo.de>
2019-12-21 23:30:04 +0100

    Encode and Decode compatibilty for Msgpack

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/midnightrun/hexagonal-architecture-url-shortener-example
 go 1.13
 
 require (
+	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	github.com/zclconf/go-cty v1.2.0
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
 	gopkg.in/dealancer/validate.v2 v2.1.0
 )
--- serializer/msgpack/msgpack.go
+++ serializer/msgpack/msgpack.go
@@ -1,16 +1,15 @@
-package json
+package msgpack
 
 import (
-	"encoding/json"
-
 	"github.com/midnightrun/hexagonal-architecture-url-shortener-example/shortener"
+	"github.com/vmihailenco/msgpack"
 )
 
 type Redirect struct{}
 
 func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
 	redirect := &shortener.Redirect{}
-	if err := json.Unmarshal(input, &redirect); err != nil {
+	if err := msgpack.Unmarshal(input, redirect); err != nil {
 		return nil, err
 	}
 
@@ -18,7 +17,7 @@ func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
 }
 
 func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
-	rawMsg, err := json.Marshal(input)
+	rawMsg, err := msgpack.Marshal(input)
 
 	if err != nil {
 		return nil, err
