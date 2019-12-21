e4b4137ac515d511e1744201549c64856615353b
Camal Cakar <camal_cakar@yahoo.de>
2019-12-21 23:22:57 +0100

    Encode and Decode compatibilty for JSON

--- serializer/json/serializer.go
+++ serializer/json/serializer.go
@@ -1,11 +1,29 @@
 package json
 
 import (
+	"encoding/json"
+
 	"github.com/midnightrun/hexagonal-architecture-url-shortener-example/shortener"
 )
 
 type Redirect struct{}
 
 func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
-	return nil, nil
+	redirect := &shortener.Redirect{}
+	if err := json.Unmarshal(input, &redirect); err != nil {
+		return err
+	}
+
+	return redirect, nil
+}
+
+func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
+	rawMsg, err := json.Marshal(input)
+
+	if err != nil 
+	{
+		return nil, err
+	}
+
+	return rawMsg, nil
 }
