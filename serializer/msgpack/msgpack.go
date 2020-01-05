1f09e03c03b11bf7a89052a329ff189023221d5f
Camal Cakar <camal_cakar@yahoo.de>
2020-01-05 16:32:30 +0100

    Error wrapping within MsgPack serializer

--- serializer/msgpack/msgpack.go
+++ serializer/msgpack/msgpack.go
@@ -1,6 +1,8 @@
 package msgpack
 
 import (
+	"fmt"
+
 	"github.com/midnightrun/hexagonal-architecture-url-shortener-example/shortener"
 	"github.com/vmihailenco/msgpack"
 )
@@ -10,7 +12,7 @@ type Redirect struct{}
 func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
 	redirect := &shortener.Redirect{}
 	if err := msgpack.Unmarshal(input, redirect); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("MsgPack Serializer: Decode %v -> %w", redirect, err)
 	}
 
 	return redirect, nil
@@ -20,7 +22,7 @@ func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
 	rawMsg, err := msgpack.Marshal(input)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("MsgPack Serializer: Encode %v -> %w", rawMsg, err)
 	}
 
 	return rawMsg, nil
