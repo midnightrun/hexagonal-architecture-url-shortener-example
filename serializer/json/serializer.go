f7f993c2a6bc50f0743065d3a562180c202bd4ed
Camal Cakar <camal_cakar@yahoo.de>
2020-01-05 16:31:17 +0100

    Error wrapping within JSON serializer

--- serializer/json/serializer.go
+++ serializer/json/serializer.go
@@ -2,6 +2,7 @@ package json
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/midnightrun/hexagonal-architecture-url-shortener-example/shortener"
 )
@@ -11,8 +12,7 @@ type Redirect struct{}
 func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
 	redirect := &shortener.Redirect{}
 	if err := json.Unmarshal(input, &redirect); err != nil {
-		return nil, fmt.Errorf("JSON Serializer: 
-		return nil, err
+		return nil, fmt.Errorf("JSON Serializer: Decode %v -> %w", redirect, err)
 	}
 
 	return redirect, nil
@@ -22,7 +22,7 @@ func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
 	rawMsg, err := json.Marshal(input)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("JSON Serializer: Encode %v -> %w", rawMsg, err)
 	}
 
 	return rawMsg, nil
